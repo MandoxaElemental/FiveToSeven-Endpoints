4e9810ed570430f5b10288244f9bfabdf6728ed7
MandoxaElemental <gprcards@gmail.com>
2025-01-08 14:45:03 -0800

    Just finished the alphanumeric reverse it

--- Controllers/ReverseItAlphanumericController.cs
+++ Controllers/ReverseItAlphanumericController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FiveToSeven_Endpoints.Services;
 
 namespace FiveToSeven_Endpoints.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
-    public class ReverseItAlphanumericController : ControllerBase
+    [Route("[controller]")]
+    public class ReverseItAlphanumeric : ControllerBase
     {
-        
+        private ReverseItAlphanumericServices _reverseItAlphanumericServices;
+        public ReverseItAlphanumeric(ReverseItAlphanumericServices reverseItAlphanumericServices){
+            _reverseItAlphanumericServices = reverseItAlphanumericServices;
+        }
+        [HttpPost]
+        [Route("reversedWord/{userInput}")]
+         public List<string> ReversedWord(string userInput){
+            return _reverseItAlphanumericServices.ReversedWord(userInput);
+         }
     }
 }
--- Services/ReverseItAlphanumericServices.cs
+++ Services/ReverseItAlphanumericServices.cs
@@ -8,12 +8,10 @@ namespace FiveToSeven_Endpoints.Services
 {
     public class ReverseItAlphanumericServices : ControllerBase
     {
-        public List<string> reversedWord = new();
+        public List<string> reversedWord = new ();
         string emptyWord = string.Empty;
-        public List<string> ReversedWord(string userInput)
-        {
-            for (int i = userInput.Length; i >= 0; i--)
-            {
+        public List<string> ReversedWord(string userInput){
+            for(int i=userInput.Length-1; i >= 0; i--){
                 emptyWord += userInput[i];
             }
             reversedWord.Add(emptyWord);
